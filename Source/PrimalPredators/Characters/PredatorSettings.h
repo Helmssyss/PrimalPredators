ab4f217755d164de0852cc73a3a55f436bc9b153
Helmssyss <84701901+Helmssyss@users.noreply.github.com>
2024-05-19 15:40:31 +0300

    Update PredatorSettings.h

--- Source/PrimalPredators/Characters/PredatorSettings.h
+++ Source/PrimalPredators/Characters/PredatorSettings.h
@@ -9,7 +9,7 @@ UENUM()
 enum class EPredatorType : uint8{
 	T_REX        UMETA(DisplayName="T-Rex"),
 	Velociraptor UMETA(DisplayName="Velociraptor"),
-	Pterodon       UMETA(DisplayName="Pterodon"),
+	Pterodon     UMETA(DisplayName="Pterodon"),
 	EMPTY        UMETA(DisplayName="Empty")
 };
 
